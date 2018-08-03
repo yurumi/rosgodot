745fe07bcf696ea60bc6cd4b5c03f6341e9cf705
thomske <j0xa@posteo.de>
2018-08-03 21:48:27 +0200

    Initial commit.

---
+++ Crane.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+onready var Body = get_node('Body')
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	# Body.add_force(Vector3(-10, 0, 0), Vector3(0, 0, 0))
+	pass
---
+++ GantryCraneRos.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _physics_process(delta):
+	# TestBody.add_force(Vector3(0, force, 0), Vector3(0, 0, 0))
+	pass
